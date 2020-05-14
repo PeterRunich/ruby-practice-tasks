b1a73fad159095f8a932101cc467b98e6536ba4c
posharnick <p.runich@bk.ru>
2020-05-14 19:30:00 +0300

    refactoring

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,9 +1,11 @@
 def caesar_cipher(string, shift)
   caesar_string = ""
+
   string.each_char do |letter|
-    ("a".."z").include?(letter.downcase) ? shift.times { letter = letter.next } : letter
+    ("a".."z").include?(letter.downcase) && shift.times { letter = letter.next }
     caesar_string << letter[-1]
   end
+
   caesar_string
 end
 
