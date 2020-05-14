9d5b1888cfe7708a821eb020a2917a234cebe424
posharnick <p.runich@bk.ru>
2020-05-14 15:15:07 +0300

    add caesar cipher task

---
+++ caesar_cipher.rb
@@ -0,0 +1,10 @@
+def caesar_cipher(string, shift)
+  caesar_string = ""
+  string.each_char do |letter|
+    ("a".."z").include?(letter.downcase) ? shift.times { letter = letter.next } : letter
+    caesar_string << letter[-1]
+  end
+  caesar_string
+end
+
+puts caesar_cipher('What a string!', 5)
